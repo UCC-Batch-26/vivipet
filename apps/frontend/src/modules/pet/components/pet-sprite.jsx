5fe0024593317a2b20c1166c0f77c3a30b120108
Jesus <jbcaponga@gmail.com>
2025-10-02 09:55:36 +0800

    feat: Change to gif, fix #58

--- apps/frontend/src/modules/pet/components/pet-sprite.jsx
+++ apps/frontend/src/modules/pet/components/pet-sprite.jsx
@@ -1,17 +1,12 @@
-import { useState, useEffect } from 'react';
 import { IMAGES } from '@/assets/images';
 import styles from './pet-sprite.module.css';
 
-export function PetSprite({ type = 'dog', activity = 'idle', mood = 'calm', speed = 900 }) {
-  const [frame, setFrame] = useState(0);
-
+export function PetSprite({ type = 'dog', activity = 'idle', mood = 'calm' }) {
   const petFrames = {
     dog: {
       walking: [IMAGES.dogWalkingGif],
       hungry: [IMAGES.hungryDog],
       dirty: [IMAGES.dirtyDog],
-      // sad: [IMAGES.dogSad1, IMAGES.dogSad2, IMAGES.dogSad3],
-      // playing: [IMAGES.dogPlaying],
       feedAction: [IMAGES.eatingDog],
       showerAction: [IMAGES.dogShower],
       playAction: [IMAGES.dogPlaying],
@@ -20,8 +15,6 @@ export function PetSprite({ type = 'dog', activity = 'idle', mood = 'calm', spee
       walking: [IMAGES.defaultCatMood],
       hungry: [IMAGES.hungryCat],
       dirty: [IMAGES.dirtyCat],
-      // sad: [IMAGES.catSad1, IMAGES.catSad2],
-      // playing: [IMAGES.catPlay1, IMAGES.catPlay2],
       feedAction: [IMAGES.eatingCat],
       showerAction: [IMAGES.catShower],
       playAction: [IMAGES.catPlaying],
@@ -30,8 +23,6 @@ export function PetSprite({ type = 'dog', activity = 'idle', mood = 'calm', spee
       walking: [IMAGES.defaultBunnyMood],
       hungry: [IMAGES.hungryBunny],
       dirty: [IMAGES.dirtyBunny],
-      // sad: [IMAGES.bunnySad1, IMAGES.bunnySad2],
-      // playing: [IMAGES.bunnyPlay1, IMAGES.bunnyPlay2],
       feedAction: [IMAGES.eatingBunny],
       showerAction: [IMAGES.bunnyShower],
       playAction: [IMAGES.bunnyPlaying],
@@ -43,7 +34,7 @@ export function PetSprite({ type = 'dog', activity = 'idle', mood = 'calm', spee
     happy: 'walking',
     hungry: 'hungry',
     dirty: 'dirty',
-    sad: 'sad',
+    sad: 'walking',
   };
 
   const actionMap = {
@@ -56,23 +47,9 @@ export function PetSprite({ type = 'dog', activity = 'idle', mood = 'calm', spee
 
   const frames = petFrames[type]?.[key] || petFrames[type]?.walking;
 
-  // console.log({ activity, mood, key, frames });
-
-  useEffect(() => {
-    if (!frames || frames.length === 1) return;
-
-    const interval = setInterval(() => {
-      setFrame((prev) => (prev + 1) % frames.length);
-    }, speed);
-
-    return () => clearInterval(interval);
-  }, [frames, speed]);
-
   return (
     <div className={styles.petSprite}>
-      {frames && <img src={frames[frame]} alt={`${type}`} className={styles.spriteImage} />}
-      {/* <p>Mood: {mood}</p>
-       <p>Activity: {activity}</p> */}
+      {frames && <img src={frames[0]} alt={type} className={styles.spriteImage} />}
     </div>
   );
 }
